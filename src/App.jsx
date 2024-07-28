dfc3f0b30839793469ca360af0da1cb48d635b8e
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-07-28 23:15:15 +0400

    :heavy_check_mark: lesson-02 code

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useEffect, useRef } from "react";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import SceneView from "@arcgis/core/views/SceneView";
+
+function App() {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const map = new Map({
+      basemap: "osm",
+    });
+
+    const mapView = new MapView({
+      map: map,
+      // container: mapRef.current,
+      zoom: 13,
+      center: [55, 25],
+    });
+
+    const sceneView = new SceneView({
+      map: new Map({
+        basemap: "navigation-dark-3d",
+      }),
+      container: mapRef.current,
+    });
+
+    return () => mapView && mapView.destroy();
+  }, []);
+
+  return (
+    <>
+      <div ref={mapRef} className="mapDiv"></div>
+    </>
+  );
+}
+
+export default App;
